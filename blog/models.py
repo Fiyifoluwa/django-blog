ef0d1c6cccafedd5c8bc341a31b4235223f5b3eb
Fiyifoluwa <fiyifoluwa@outlook.com>
2021-04-27 10:21:14 +0100

    comments

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import AbstractUser, User
 
 class Post(models.Model):
   title = models.CharField(max_length=200)
@@ -10,4 +11,18 @@ class Post(models.Model):
     return self.title
 
   def get_absolute_url(self):
-    return reverse('post_detail', args=[str(self.id)])
+    return reverse('post_detail')
+
+class Comment(models.Model): # new
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  comment = models.CharField(max_length=140)
+  author = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE,
+  )
+
+  def __str__(self):
+    return self.comment
+
+  def get_absolute_url(self):
+    return reverse('postDetail')
