e59630bee1daf2c1a4d4c5db6b55b1b1881f8d89
Fiyifoluwa <fiyifoluwa@outlook.com>
2021-04-27 11:07:54 +0100

    email

--- config/settings.py
+++ config/settings.py
@@ -147,4 +147,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 django_heroku.settings(locals())
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
